576b57847e6b9963a38fb4574c0d6e511c2744e7
NayamaRosa <falecom@nayama.com.br>
2021-11-13 22:01:21 -0300

    getting api on stores list container

--- src/routes/StoresList/StoresList.jsx
+++ src/routes/StoresList/StoresList.jsx
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const StoresList = () => {
+  const [api, setApi] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        await fetch('https://api.gruposbf.com.br/geolocation-api/stores')
+        .then((res) => res.json())
+        .then((data) => setApi(data))
+      } catch (e) {
+        console.error(e);
+        throw new Error("Serviço indisponível, tente novamente mais tarde")
+      }
+    };
+    fetchData();
+  }, [])
+  console.log(api);
+
   return (
     <p>stores</p>
   );
