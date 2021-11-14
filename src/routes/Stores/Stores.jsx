7b2d2cfa5d19432104b23eac2f229450cb6eb972
NayamaRosa <falecom@nayama.com.br>
2021-11-14 16:42:13 -0300

    creating stores route

---
+++ src/routes/Stores/Stores.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+import StoresList from '../../containers/StoresList';
+
+const Stores = () => {
+  return (
+    <main class="container">
+      <StoresList />
+    </main>
+  )
+}
+
+export default Stores;
