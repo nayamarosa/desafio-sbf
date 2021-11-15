c8cb3a2413be26c8ad309148e5bce66c74bb22c7
NayamaRosa <falecom@nayama.com.br>
2021-11-14 23:27:22 -0300

    adding stores list and items style

--- src/components/StoresItem/StoresItem.jsx
+++ src/components/StoresItem/StoresItem.jsx
@@ -2,11 +2,16 @@ import React from "react";
 
 const StoresItem = ({ name, adress }) => {
   return (
-    <div>
-      <p>{name}</p>
-      <p>{adress}</p>
-      <p>horário de funcionamento</p>
-    </div>
+    <li class="stores__list-item">
+      <p class="stores__list-item--title">{name}</p>
+      <p class="stores__list-item--map">Ver no mapa</p>
+      <div class="stores__list-item-group">
+        <p class="stores__list-item--address">{adress}</p>
+        <p class="stores__list-item--time">Atendimento:</p>
+        <p class="stores__list-item--time">Segunda a Sábado 10h às 22h | Domingo 11h às 20h</p>
+        <span>Disponível em 4 dias úteis</span>
+      </div>
+    </li>
   )
 }
 
