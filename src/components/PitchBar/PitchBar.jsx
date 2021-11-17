1a54d86a8d72d80d48aae3555259896e30da08f7
NayamaRosa <falecom@nayama.com.br>
2021-11-17 17:54:03 -0300

    adding pitchbar arrows

--- src/components/PitchBar/PitchBar.jsx
+++ src/components/PitchBar/PitchBar.jsx
@@ -4,11 +4,17 @@ import Icon from '../Icon';
 
 const PitchBar = () => {
   return (
-    <section className="pitchbar">
-      <div className="container pitchbar__group">
+    <section className="container pitchbar">
+      <a href="/" >
+        <Icon icon="./images/icon_seta_esquerda.svg" alt="Anterior" />
+      </a>
+      <div className="pitchbar__group">
         <Icon icon="./images/logo_nike_snkrs.svg" alt="Logo Nike Sneakers" />
         <p>Fique por dentro dos lançamentos <span>SNKRS</span></p>
       </div>
+      <a href="/" >
+        <Icon icon="./images/icon_seta_direita.svg" alt="Próximo" />
+      </a>
     </section>
   )
 }
