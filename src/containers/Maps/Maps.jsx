0861bb895c2aeceb59b7a420753fdcca7ac003dd
NayamaRosa <falecom@nayama.com.br>
2021-11-16 22:58:49 -0300

    adjusting new map style

--- src/containers/Maps/Maps.jsx
+++ src/containers/Maps/Maps.jsx
@@ -6,16 +6,18 @@ export const Maps = (props) => {
   const coordinates = props.coordinates.split(',');
   const onlyCoordinates = [];
   props.closests.map(item => onlyCoordinates.push([item.latitude, item.longitude]))
+
   
   const displayMarkers = () => {
-    return onlyCoordinates.map((store, index) => {
+    return props.closests.map((store, index) => {
       return <Marker key={index} id={index} position={{
-        lat: store[0],
-        lng: store[1]
+        lat: store.latitude,
+        lng: store.longitude
       }}
       icon={{
         url: './images/icon_pin_mapa.svg'
       }}
+      title={store.name}
       />
     })
   }
@@ -25,7 +27,6 @@ export const Maps = (props) => {
           google={props.google}
           zoom={12}
           initialCenter={{ lat: coordinates[0], lng: coordinates[1] }}
-          icon={props.path}
         >
           {displayMarkers()}
         </Map>
